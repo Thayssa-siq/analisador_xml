d84812b8180d3147967d217cc53596fb8e19dc46
Thayssa-Siq <thayssasiqueira15@gmail.com>
2025-05-10 11:45:00 -0300

    Adiciona formulÃ¡rio e template para upload de XMLs

---
+++ uploadxml/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class XMLUploadForm(forms.Form):
+    xml_files = forms.FileField(
+        label='Selecione um ou mais arquivos XML de NF-e/CT-e',
+        required=True
+    )
+
+# TODO: Validar extensões dos arquivos XML
